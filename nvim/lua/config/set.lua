d17f95f490c42bf66a3bd3a8d0913df75b76367c
mateusribeirog <matribg04@gmail.com>
2025-11-14 19:46:35 -0300

    changes

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,4 +1,8 @@
---vim.opt.guicursor = ""
+vim.o.guicursor =
+    "n-v-c-sm:block-blinkwait175-blinkon175-blinkoff150," ..
+    "i-ci-ve:ver25-blinkwait175-blinkon175-blinkoff150," ..
+    "r-cr-o:hor20"
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -6,7 +10,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-    
+
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
@@ -19,11 +23,11 @@ vim.opt.wrap = false
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
 vim.opt.updatetime = 50
-vim.opt.termguicolors=true
+vim.opt.termguicolors = true
 --vim.opt.winblend = 0
 vim.opt.laststatus = 2
 --vim.opt.background='dark'
 vim.opt.fillchars = {
-    stl = "─" 
+    stl = "─"
 }
-vim.opt.guifont="JetBrainsMono Nerd Font"
+vim.opt.guifont = "JetBrainsMono Nerd Font"
---
+++ zsh/devcontainer.zsh
@@ -0,0 +1,69 @@
+#!/bin/env bash
+
+dvc-build () {
+    devcontainer build \
+        --workspace-folder . \
+        --log-level debug
+
+}
+
+
+get_container_user() {
+    jq -r '.containerUser // ""' .devcontainer/devcontainer.json
+}
+
+dvc-up () {
+    flags=()
+    user_name=""
+
+    # Parse arguments
+    for arg in "$@"; do
+        case "$arg" in
+            --*) # If it starts with --, treat as flag
+                flags+=("$arg")
+                ;;
+            *) # Otherwise assume it's the username
+                user_name="$arg"
+                ;;
+        esac
+    done
+
+    if [ -z "$user_name" ]; then
+        # fallback to helper or default
+        user_name=$(get_container_user)
+    fi
+    if [ -z "$user_name" ]; then
+        user_name="root"
+    fi
+
+    echo "Container user is $user_name"
+    echo "Leading flags: ${flags[*]}"
+
+    devcontainer up \
+        "${flags[@]}" \
+        --workspace-folder . \
+        --log-level debug \
+        --gpu-availability all  \
+        --mount "type=bind,source=$HOME/.config/nvim,target=/home/$user_name/.config/nvim" \
+        --mount "type=bind,source=$HOME/.local/share/nvim,target=/home/$user_name/.local/share/nvim" \
+        --mount "type=bind,source=$HOME/.local/state/nvim,target=/home/$user_name/.local/state/nvim" \
+        --mount "type=bind,source=$HOME/.cache/wal,target=/home/$user_name/.cache/wal" \
+        --mount "type=bind,source=/tmp/tmux-1000/,target=/tmp/tmux-1000/" \
+        --mount "type=bind,source=$HOME/.gitconfig,target=/home/$user_name/.gitconfig" \
+        --mount "type=bind,source=$HOME/.ssh,target=/home/$user_name/.ssh" \
+        --mount "type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix" \
+        --mount "type=bind,source=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY,target=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" \
+        --remote-env "GIT_SSH_COMMAND=ssh -o StrictHostKeyChecking=no" \ 
+        --remote-env "DISPLAY=$DISPLAY" \ 
+        --remote-env "WAYLAND_DISPLAY=$WAYLAND_DISPLAY" \ 
+        --remote-env "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \ 
+        --remote-env "GIT_SSH_COMMAND=ssh -o StrictHostKeyChecking=no" \
+        --remote-env "DISPLAY=$DISPLAY" \
+        --additional-features '{
+            "ghcr.io/devcontainers/features/git:1": {},
+            "ghcr.io/devcontainers-extra/features/fzf:1": {},
+            "ghcr.io/duduribeiro/devcontainer-features/neovim:1": {},
+            "ghcr.io/duduribeiro/devcontainer-features/tmux:1": {},
+            "ghcr.io/devcontainers/features/node:1": { "version": "lts" }
+        }'
+}
